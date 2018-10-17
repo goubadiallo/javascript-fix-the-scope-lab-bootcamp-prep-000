943a8ff703f73cc507f3350260595dbd5065c8be
Mamadou Diallo <mamadou.diallo@wework.com>
2018-10-17 21:25:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function funkyFunction(){
     return 'FUNKY!'
   }
 }
-console.log("")
+console.log("FUNKY!")
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
