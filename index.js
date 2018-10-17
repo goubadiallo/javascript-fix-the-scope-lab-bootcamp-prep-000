ba0e011c8a24843f0eeea058053d9036ab8beb8b
Mamadou Diallo <mamadou.diallo@wework.com>
2018-10-17 21:33:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function funkyFunction(){
   funkyFunction()
 
 }
-
+var funkyFunction = theFunk()
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
